dfa608dfe8a90c6185f7dfa78f9f4caac5258609
edvalerawork <edvalerawork@users.noreply.github.com>
2025-12-30 10:17:57 -0500

    update 30 dic v.2

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,21 +26,3 @@ const email = `${p1}.${p2}.${p3}@${p4}.com`;
 document.getElementById("email").innerHTML = `
   <a href="mailto:${email}">${email}</a>
 `;
-
-
-/* ================= SERVICIOS - TOGGLE CARDS ================= */
-
-document.querySelectorAll('.service-card').forEach(card => {
-  card.addEventListener('click', () => {
-
-    // cerrar las demás tarjetas
-    document.querySelectorAll('.service-card').forEach(other => {
-      if (other !== card) {
-        other.classList.remove('active');
-      }
-    });
-
-    // alternar la actual
-    card.classList.toggle('active');
-  });
-});
